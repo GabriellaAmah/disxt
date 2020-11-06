29b7b9c67310b2bf18a961753985557f726c7be6
Iyosayi <Iyosa14@gmail.com>
2020-11-06 08:24:14 +0100

    updated app.js

--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,4 @@
 import express from 'express'
-import redis from 'redis'
-
-const client = redis.createClient({
-  host: 'redis',
-  port: 6379
-})
-
-client.set('visits', 0)
 
 const app = express()
 
@@ -14,11 +6,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (_, res) => {
-  client.get('visits', (err, visits) => {
-    if (err) console.log(err)
-    res.json({ visits })
-    client.set('visits', parseInt(visits) + 1)
-  })
+  res.json({ msg: 'Hello Disxt.' })
 })
 
 require('../routes')(app)
